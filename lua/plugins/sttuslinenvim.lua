99531570be50a21662be16c6a17e1286dd14a7c2
yofreOrmaza <ormaza.main@gmail.com>
2025-02-27 12:58:59 -0500

    chore: Add github copilot icons to sttatusline function

--- lua/plugins/sttuslinenvim.lua
+++ lua/plugins/sttuslinenvim.lua
@@ -1,17 +1,30 @@
- require("sttusline").setup ({
-     components = {
-         "mode",
-         "filename",
-         "git-branch",
-         "git-diff",
-         "%=",
-         "datetime",
-         "diagnostics",
-         "lsps-formatters",
-         "copilot",
-         "indent",
-         "encoding",
-         "pos-cursor",
-         "pos-cursor-progress",
-     },
- })
+require("sttusline").setup ({
+    components = {
+        "mode",
+        "filename",
+        "git-branch",
+        "git-diff",
+        "%=",
+        "datetime",
+        "diagnostics",
+        "lsps-formatters",
+        "copilot",
+        "indent",
+        "encoding",
+        "pos-cursor",
+        "pos-cursor-progress",
+    },
+})
+
+local copilot = require("sttusline.components.copilot")
+
+copilot.set_colors { fg = "#ECE81A" }
+
+copilot.set_config {
+    icons = {
+        normal = "",
+        error = "",
+        warning = "",
+        inprogress = "",
+    },
+}
