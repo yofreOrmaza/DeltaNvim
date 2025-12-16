ab2ba6e72e5a4cc5313793f082f5eef5659f24a4
yofreOrmaza <socialnetworks.mollusk730@simplelogin.com>
2025-12-16 00:45:10 -0500

    fix(nvim-lspconfig): Update/Replace with vim.lsp module
    Replace previous content with require with the base example
    vim.lsp.config('luals', {
      cmd = {'lua-language-server'},
      filetypes = {'lua'},
      root_markers = {'.luarc.json', '.luarc.jsonc'},
    })
    
    vim.lsp.enable('luals')
    Closes: #33

--- lua/plugins/l-lspzero.lua
+++ lua/plugins/l-lspzero.lua
@@ -1,27 +1,8 @@
-local lsp = require('lsp-zero').preset({
-    name = 'minimal',
-    set_lsp_keymaps = true,
-    manage_nvim_cmp = true,
-    suggest_lsp_servers = false,
+---@diagnostic disable-next-line: undefined-global
+vim.lsp.config('luals', {
+  cmd = {'lua-language-server'},
+  filetypes = {'lua'},
+  root_markers = {'.luarc.json', '.luarc.jsonc'},
 })
 
--- (Optional) Configure lua language server for neovim
-lsp.nvim_workspace()
-
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = false,
-    signs = false,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = true,
-    float = {
-        focusable = false,
-        style = 'minimal',
-        border = 'none',
-        source = 'always',
-        header = '',
-        prefix = '',
-    }
-})
+vim.lsp.enable('luals')
